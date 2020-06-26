04dc4b02086485d1a5d8c4d17b5360f86aa948f4
Glyan <pamela27jr@gmail.com>
2020-06-26 12:21:57 +0100

    Check and delete todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,22 @@
 <template>
-    <div class="todo-item">
-        <p>{{todo.title}}</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">X</button>    
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete () {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
 }
 </script>
 
@@ -17,9 +26,11 @@ export default {
         padding: 10px;
         border-bottom: 1px #ccc dotted;
     }
+
     .is-complete {
         text-decoration: line-through;
     }
+
     .del {
         background: #ff0000;
         color: #fff;
